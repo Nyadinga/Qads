bbc716602038468fd2a786bb69ee2f9edfb9f609
kinyuy kelly <kinyuykelly213@gmail.com>
2026-04-22 16:11:14 +0100

    added campaign relationships

--- Backend/src/modules/Campaign/models/index.js
+++ Backend/src/modules/Campaign/models/index.js
@@ -1,19 +1,23 @@
 const Campaign = require("./campaign.Model");
 const User = require("../../Authentication/models/user.Model");
 const CampaignMedia = require("./campaignMedia.Model");
+const CampaignLink = require("./campaignLink.Model");
 
 User.hasMany(Campaign, {
     foreignKey: 'user_id',
-    as: 'campaigns' });
+    as: 'campaigns'
+});
 
 
 Campaign.belongsTo(User, {
     foreignKey: 'user_id',
-    as: 'owner' });
+    as: 'owner'
+});
 
 Campaign.belongsTo(User, {
      foreignKey: 'reviewed_by', 
-     as: 'reviewer' });
+     as: 'reviewer'
+});
 
 Campaign.hasMany(CampaignMedia, { 
     foreignKey: "campaign_id", 
@@ -24,9 +28,44 @@ CampaignMedia.belongsTo(Campaign, {
     as: "campaign"
  });
 
+Campaign.hasMany(CampaignLink, { 
+  foreignKey: 'campaign_id', 
+  as: 'generated_links' 
+});
+
+CampaignLink.belongsTo(Campaign, { 
+  foreignKey: 'campaign_id', 
+  as: 'campaign' 
+});
+
+User.hasMany(CampaignLink, { 
+  foreignKey: 'promoter_id', 
+  as: 'my_promoter_links' 
+});
+
+CampaignLink.belongsTo(User, { 
+  foreignKey: 'promoter_id', 
+  as: 'promoter' 
+});
+
+Campaign.belongsToMany(User, {
+  through: CampaignLink,
+  foreignKey: 'campaign_id',
+  otherKey: 'promoter_id',
+  as: 'promoters' 
+});
+
+User.belongsToMany(Campaign, {
+  through: CampaignLink,
+  foreignKey: 'promoter_id',
+  otherKey: 'campaign_id',
+  as: 'promoted_campaigns' 
+});
+
 
 module.exports = {
   Campaign,
   User,
-  CampaignMedia
+  CampaignMedia,
+  CampaignLink
 };
